ae03f165da6beb52c218d787345efde8d8ca6b2b
Mikey <86084001+simonelprogramas@users.noreply.github.com>
2021-06-19 04:04:39 -0300

    Me re mame man
    aaaaaaaaaaaaaaaaaaaaaa

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("god head nazi")
+$(document).ready(function(){
+
+    $(window).scroll(function(){
+        if( $(this).scrollTop() > 0 ){
+            $("header").addClass("header2");
+        } else {
+            $("header").removeClass("header2");
+        }
+    });
+
+});
